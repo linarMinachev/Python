d36b6627f56e74326c690f9e86dcdf2d996e8827
blindstudio <blindstudio1@gmail.com>
2022-10-05 12:53:04 +0300

    done

--- sem_3/task_1.py
+++ sem_3/task_1.py
@@ -7,13 +7,11 @@ from random import randint
 
 len_array = int(input("Введите сколько чисел должно быть в списке: "))
 numbers = []
+sum_elements = 0
+
 for i in range(len_array):
     numbers.append(randint(1, 10))
-i = -1
-sum_elements = 0
-while i < len(numbers)-1:
-    i += 1
-    if i % 2 != 0:
-        sum_elements += numbers[i]
-numbers_odd = numbers[1::2]
-print(f"Созданный список -> {numbers}\nЭлементы на нечетных позициях -> {numbers_odd}\nСумма этих элементов -> {sum_elements}")
+for val in numbers[1::2]:
+    sum_elements += val
+
+print(f"Созданный список -> {numbers}\nСумма этих элементов -> {sum_elements}")
