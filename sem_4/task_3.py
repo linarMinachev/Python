efec292d6edb4aa80e08b52cf14a5eae41b2b6b8
blindstudio <blindstudio1@gmail.com>
2022-10-10 18:14:29 +0300

    done

---
+++ sem_4/task_3.py
@@ -0,0 +1,22 @@
+# Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся
+# элементов исходной последовательности.
+
+from random import randint
+
+
+def create_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+
+def get_unic_value(list):
+    return [i for i in set(list)]
+
+
+size = 10
+m = 1
+n = 10
+
+origin = create_list(size, m, n)
+print(origin)
+print(get_unic_value(origin))
